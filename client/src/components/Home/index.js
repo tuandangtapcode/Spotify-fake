98657cd89e2aca15440ae7023d2d7607be22cd4c
tuanpm <tuanpham081102@gmail.com>
2023-11-04 10:09:39 +0700

    test

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -20,7 +20,7 @@ const Home = ({ children }) => {
         <FooterStyled className='d-flex-sb mt-8'>
           <div>
             <p className="">XEM TRƯỚC SPOTIFY</p>
-            <p>Đăng ký để nghe không giới hạn các bài hát và podcast với quảng cáo không thường xuyên. Không cần thẻ tín dụng</p>
+            <p>Đăng ký để nghe không giới hạn các bài hát và podcast với quảng cáo không thường xuyên. Không cần thẻ tín dụngfghnsfnsfg</p>
           </div>
 
           <button className="fs-16 fw-600">Đăng ký miễn phí</button>
